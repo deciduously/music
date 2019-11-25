ef17667157d284c5df21324995e53d75569c8f98
deciduously <bendlovy@gmail.com>
2019-11-25 08:19:56 -0500

    Fix playback

--- src/bin/mod.rs
+++ src/bin/mod.rs
@@ -1,9 +1,11 @@
 use music::*;
+use rodio::{Sink, source::SineWave, default_output_device};
 
 fn main() {
-    //let device = default_output_device().unwrap();
-    //let sink = Sink::new(&device);
-    //let source = SineWave::from(Pitch::default());
-    //sink.append(source);
     println!("{}", GREETING);
+    let device = default_output_device().unwrap();
+    let sink = Sink::new(&device);
+    let source = SineWave::from(Pitch::default());
+    sink.append(source);
+    sink.sleep_until_end();
 }
